b7ed9da5b0d75f85d9310317c0b2a68bcf7f7754
Ameer Iyad AL-Hopi <78345668+Dev-Ameer@users.noreply.github.com>
2022-12-20 19:23:23 +0200

    Add files via upload

---
+++ js/jq.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    // $(".content").hover(function () {
+    //         $(".i").css("margin-top", "10px");
+            
+    //     }, function () {
+    //         $(".i").css("margin-top", "-10px");
+    //     }
+    // );
+
+
+
+});
